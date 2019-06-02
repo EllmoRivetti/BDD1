75a88b934f08567132da12fe48e7ad32e6b8acad
Ellmo Rivetti <ellmorivetti@gmail.com>
2019-06-02 16:01:50 +0200

    sql querry for backend

--- src/main/java/App/View/BackEndController.java
+++ src/main/java/App/View/BackEndController.java
@@ -1,7 +1,11 @@
 package App.View;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import com.jfoenix.controls.JFXListView;
 
+import App.Model.DatabaseManager;
 import javafx.fxml.FXML;
 
 public class BackEndController {
@@ -19,4 +23,38 @@ public class BackEndController {
 
 	@FXML
 	private JFXListView<String> listVehiculesNonDispo;
+	
+	
+	@FXML
+	public void initialize() {
+		fillLists();
+	}
+
+	
+	private void fillLists() {
+		try {
+			ResultSet resultLivreurLibre = DatabaseManager.executeQuerry("SELECT * FROM livreur WHERE statut = 0");
+			ResultSet resultVehiculeAndLivreurNonDispo = DatabaseManager.executeQuerry("SELECT v.nom, l.nom FROM vehicule v , livreur l WHERE l.statut = 1 and l.vehicule = v.idVehicule");
+			ResultSet resultVehiculeDispo = DatabaseManager.executeQuerry("SELECT * FROM vehicule v where not exists (SELECT * FROM vehicule x , livreur l WHERE v.nom = x.nom and l.statut = 1 and l.vehicule = v.idVehicule)");
+			
+			while(resultLivreurLibre.next()) {//Fill listLivreursDispo
+				listLivreursDispo.getItems().add(resultLivreurLibre.getObject("nom").toString());
+			}
+			
+			while(resultVehiculeAndLivreurNonDispo.next()) {
+				listVehiculesNonDispo.getItems().add(resultVehiculeAndLivreurNonDispo.getObject(1).toString());
+				String s = resultVehiculeAndLivreurNonDispo.getObject(2).toString() + " utilisant le véhicule "+resultVehiculeAndLivreurNonDispo.getObject(1).toString();
+				listLivreursNonDispo.getItems().add(s);
+			}
+			
+			while(resultVehiculeDispo.next()) {
+				listVehiculesDispo.getItems().add(resultVehiculeDispo.getObject("nom").toString());
+			}	
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
 }
